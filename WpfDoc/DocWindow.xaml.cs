73ab086754990f80793871853f12412cb39e9030
greenviper206 <toddtsai206@gmail.com>
2025-11-26 11:08:54 +0800

    增強 DocWindow 編輯功能並優化 UI 元件命名
    - 工具列按鈕與選擇器新增事件綁定，操作更直覺
    - RichTextBox 加入名稱與選取事件，支援即時格式同步
    - 新增開新檔案、開啟檔案、儲存檔案（RTF）功能
    - 實作刪除全部內容功能
    - 修正 UI 元件命名（如斜體按鈕改為 Italic）
    - Yee2 工具列寬度由 300 調整為 416
    - 引入必要命名空間（Microsoft.Win32、System.IO）
    - 工具列可即時調整選取文字的字型、大小、顏色

--- WpfDoc/DocWindow.xaml.cs
+++ WpfDoc/DocWindow.xaml.cs
@@ -1,10 +1,13 @@
-﻿using System;
+﻿using Microsoft.Win32;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
 using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
@@ -38,18 +41,93 @@ namespace WpfDoc
         }
 
         private void NewFile_Executed(object sender, ExecutedRoutedEventArgs e)
-        { 
-
+        {
+            DocWindow docWindow = new DocWindow();
+            docWindow.Show();
         }
 
-        private void OpenFile_Executed(object sender, ExecutedRoutedEventArgs e)
+        private void OpenFile_Executed(object sender, System.Windows.Input.ExecutedRoutedEventArgs e)
         {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            {
+                openFileDialog.Filter = "Rich Text Format (*.rtf)|*.rtf|All files (*.*)|*.*";
+                openFileDialog.DefaultExt = ".rtf";
+                openFileDialog.Multiselect = false;
+            };
 
+            if (openFileDialog.ShowDialog() == true)
+            {
+                TextRange range = new TextRange(Edit.Document.ContentStart, Edit.Document.ContentEnd);
+                FileStream fileStream = new FileStream(openFileDialog.FileName, FileMode.Open);
+                range.Load(fileStream, DataFormats.Rtf);
+                fileStream.Close();
+            }
         }
 
-        private void SaveFile_Executed(object sender, ExecutedRoutedEventArgs e)
+        private void SaveFile_Executed(object sender, System.Windows.Input.ExecutedRoutedEventArgs e)
         {
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            {
+                saveFileDialog.Filter = "Rich Text Format (*.rtf)|*.rtf|All files (*.*)|*.*";
+                saveFileDialog.DefaultExt = ".rtf";
+                saveFileDialog.AddExtension = true;
+            };
 
+            if (saveFileDialog.ShowDialog() == true)
+            {
+                TextRange range = new TextRange(Edit.Document.ContentStart, Edit.Document.ContentEnd);
+                FileStream fileStream = new FileStream(saveFileDialog.FileName, FileMode.Create);
+                range.Save(fileStream, DataFormats.Rtf);
+                fileStream.Close();
+            }
+        }
+        private void DelAll_Clicked(object sender, RoutedEventArgs e)
+        {
+            Edit.Document.Blocks.Clear();
+        }
+
+        private void Edit_SelectionChanged(object sender, RoutedEventArgs e)
+        {
+            var property_bold = Edit.Selection.GetPropertyValue(TextElement.FontWeightProperty);
+            Bold.IsChecked = (property_bold != DependencyProperty.UnsetValue) && (property_bold.Equals(FontWeights.Bold));
+
+            var property_italic = Edit.Selection.GetPropertyValue(TextElement.FontStyleProperty);
+            Italic.IsChecked = (property_italic != DependencyProperty.UnsetValue) && (property_italic.Equals(FontStyles.Italic));
+
+            var property_underline = Edit.Selection.GetPropertyValue(Inline.TextDecorationsProperty);
+            Underline.IsChecked = (property_underline != DependencyProperty.UnsetValue) && (property_underline.Equals(TextDecorations.Underline));
+
+            var property_fontfamily = Edit.Selection.GetPropertyValue(TextElement.FontFamilyProperty);
+            FontFamilyComboBox.SelectedItem = property_fontfamily;
+
+            var property_fontsize = Edit.Selection.GetPropertyValue(TextElement.FontSizeProperty);
+            FontSizeComboBox.SelectedItem = property_fontsize;
+
+            var property_fontcolor = Edit.Selection.GetPropertyValue(TextElement.ForegroundProperty);
+            FontColorPicker.SelectedColor = ((SolidColorBrush)property_fontcolor).Color;
+        }
+
+        private void FontColorPicker_SelectedColorChanged(object sender, RoutedPropertyChangedEventArgs<Color?> e)
+        {
+            fontColor = (Color)e.NewValue;
+            SolidColorBrush fontBrush = new SolidColorBrush(fontColor);
+            Edit.Selection.ApplyPropertyValue(TextElement.ForegroundProperty, fontBrush);
+        }
+
+        private void FontFamilyComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (FontFamilyComboBox.SelectedItem != null)
+            {
+                Edit.Selection.ApplyPropertyValue(TextElement.FontFamilyProperty, FontFamilyComboBox.SelectedItem);
+            }
+        }
+
+        private void FontSizeComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (FontSizeComboBox.SelectedItem != null)
+            {
+                Edit.Selection.ApplyPropertyValue(TextElement.FontSizeProperty, FontSizeComboBox.SelectedItem);
+            }
         }
     }
 }
